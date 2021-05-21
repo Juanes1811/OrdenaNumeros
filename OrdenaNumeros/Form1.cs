5739a5b275cce0700348e0e239359e7f102b7a9c
Juan Esteban Alzate quintero <juan.alzatequintero@upb.edu.co>
2021-05-20 19:30:38 -0500

    Implementacion de la clase logica

--- OrdenaNumeros/Form1.cs
+++ OrdenaNumeros/Form1.cs
@@ -6,41 +6,31 @@ namespace OrdenaNumeros
 {
     public partial class Form1 : Form
     {
-        //Atributos propios del juego
-        private int[,] matrizValores;
-        private Button[,] matrizBotones;
-        private int posicionFila, posicionColumna;
 
-        /// <summary>
-        /// Constructor de la clase Form1
-        /// </summary>
+        Logica logicaJuego;
+
+        private Button[,] matrizBotones;
+
+
         public Form1()
         {
+            logicaJuego = new Logica();
+
             InitializeComponent();
 
-            posicionFila = 0;
-            posicionColumna = 0;
-
             matrizBotones = new Button[4, 4];
-            matrizValores = new int[4, 4];
 
-            //Aqui se invocan los metodos que inicializan las matrices
+
             InicializaMatrizBotones();
-            InicializaMatrizValores();
+            logicaJuego.InicializaMatrizValores();
         }
 
-        /// <summary>
-        /// Evento que se usa cuando se carga la forma por primera vez
-        /// </summary>
         private void Form1_Load(object sender, EventArgs e)
         {
             //Se invoca la inicialización de botones
             InicializaEtiquetaBotones();
         }
 
-        /// <summary>
-        /// Inicializa la matriz de botones, con los botones del interfaz
-        /// </summary>
         private void InicializaMatrizBotones()
         {
             matrizBotones[0, 0] = boton1;
@@ -64,52 +54,10 @@ namespace OrdenaNumeros
             matrizBotones[3, 3] = boton16;
         }
 
-        /// <summary>
-        /// Inicializa la matriz de valores, asignando los numeros a organizar
-        /// </summary>
-        private void InicializaMatrizValores()
-        {
-            int valor = 0;
-
-            //Inicialmente se asignan los números del 0 al 15 en toda la matriz
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    matrizValores[i, j] = valor;
-                    valor++;
-                }
-            }
-
-            //Luego, procedemos a cambiar los valores de posición de manera aleatoria
-
-            Random aleatorio = new Random();
-            int posicionHorizontal, posicionVertical, valorTemporal;
-
-            //Aqui desordenamos la matriz, calculando posiciones horizontales y
-            //verticales dentro de la matriz
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    valorTemporal = matrizValores[i, j];
-                    posicionHorizontal = aleatorio.Next(4);
-                    posicionVertical = aleatorio.Next(4);
-
-                    matrizValores[i, j] = matrizValores[posicionHorizontal, posicionVertical];
-                    matrizValores[posicionHorizontal, posicionVertical] = valorTemporal;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Asigna los valores de la matrizValores como etiquetas de los
-        /// botones en la matrizBotones
-        /// </summary>
         private void InicializaEtiquetaBotones()
         {
             //Recalculamos la matriz de valores
-            InicializaMatrizValores();
+            logicaJuego.InicializaMatrizValores();
 
             for (int i = 0; i < 4; i++)
             {
@@ -117,10 +65,10 @@ namespace OrdenaNumeros
                 {
                     //El botón que tenga el valor 0, se verá como vacío
                     //para que el usuario pueda "desplazar" el valor allí
-                    if (matrizValores[i, j] == 0)
+                    if (logicaJuego.MatrizValores[i, j] == 0)
                         matrizBotones[i, j].Text = "";
                     else
-                        matrizBotones[i, j].Text = matrizValores[i, j].ToString();
+                        matrizBotones[i, j].Text = logicaJuego.MatrizValores[i, j].ToString();
                 }
             }
         }
@@ -239,11 +187,10 @@ namespace OrdenaNumeros
         /// <param name="datoColumna">Columna en la matriz a la que pertenece el botón</param>
         private void EvaluaBotonPresionado(int numeroBoton, int datoFila, int datoColumna)
         {
-            posicionFila = datoFila;
-            posicionColumna = datoColumna;
-
             //Aqui evaluamos en la matrizValores, la posición correspondiente al botón presionado
-            EvaluaPosicion();
+            logicaJuego.EvaluaBoton(numeroBoton, datoFila, datoColumna);
+
+            Actualizador();
 
             //Finalmente, se da la notificación si el valor se encuentra en la posición correcta
             NotificaPosicionCorrectaValor();
@@ -252,107 +199,16 @@ namespace OrdenaNumeros
         /// <summary>
         /// Evalua si la posición presionada está adjacente al espacio disponible para usar
         /// </summary>
-        private void EvaluaPosicion()
-        {
-            int valorTemporal = 0;
-
-            //Validamos el valor superior a donde presionamos si está el cero
-            if (posicionFila > 0)
-            {
-                if (matrizValores[posicionFila - 1, posicionColumna] == 0)
-                {
-                    valorTemporal = matrizValores[posicionFila, posicionColumna];
-                    matrizValores[posicionFila, posicionColumna] = 0;
-                    matrizValores[posicionFila - 1, posicionColumna] = valorTemporal;
-                }
-            }
-
-            //Validamos el valor inferior a donde presionamos si está el cero
-            if (posicionFila < 3)
-            {
-                if (matrizValores[posicionFila + 1, posicionColumna] == 0)
-                {
-                    valorTemporal = matrizValores[posicionFila, posicionColumna];
-                    matrizValores[posicionFila, posicionColumna] = 0;
-                    matrizValores[posicionFila + 1, posicionColumna] = valorTemporal;
-                }
-            }
-
-            //Validamos el valor izquierdo a donde presionamos si está el cero
-            if (posicionColumna > 0)
-            {
-                if (matrizValores[posicionFila, posicionColumna - 1] == 0)
-                {
-                    valorTemporal = matrizValores[posicionFila, posicionColumna];
-                    matrizValores[posicionFila, posicionColumna] = 0;
-                    matrizValores[posicionFila, posicionColumna - 1] = valorTemporal;
-                }
-            }
-
-            //Validamos el valor derecho a donde presionamos si está el cero
-            if (posicionColumna < 3)
-            {
-                if (matrizValores[posicionFila, posicionColumna + 1] == 0)
-                {
-                    valorTemporal = matrizValores[posicionFila, posicionColumna];
-                    matrizValores[posicionFila, posicionColumna] = 0;
-                    matrizValores[posicionFila, posicionColumna + 1] = valorTemporal;
-                }
-            }
-
-            //Finalmente actualizamos etiquetas de los botones
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    if (matrizValores[i, j] == 0)
-                        matrizBotones[i, j].Text = "";
-                    else
-                        matrizBotones[i, j].Text = matrizValores[i, j].ToString();
-                }
-            }
-
-            //Y valoramos la condición de victoria
-            EvaluaCondicionVictoria();
-        }
 
         /// <summary>
         /// Esta función valida si todos los números están organizados
         /// </summary>
         private void EvaluaCondicionVictoria()
         {
-            //Partimos del supuesto que ya logramos la condición de victoria
-            bool condicionVictoria = true;
-
-            int valorBuscado = 0;
-
-            //Aqui recorremos la matriz de valores buscando para cada posición si el valor está en orden
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    //incrementamos el valor buscado
-                    valorBuscado++;
-
-                    //Si los valores son diferentes, entonces todavía necesitamos seguir jugando!!!
-                    if (matrizValores[i, j] != valorBuscado)
-                    {
-                        // Validamos si estamos en la última casilla, el valor existente es 0,
-                        // el valor buscado ya llegó a 16 y la condición de victoria sigue siendo true
-                        if (matrizValores[i, j] == 0 && valorBuscado == 16 && condicionVictoria == true)
-                            condicionVictoria = true;
-
-                        // De lo contrario, es porque estamos en cualquier otra casilla y los valores
-                        // Todavía no son iguales
-                        else
-                            condicionVictoria = false;
-                    }
-                }
-            }
-
+            bool victoria = logicaJuego.EvaluaCondicionVictoria();
 
             //Si la condición de victoria se logró, mostramos el mensaje de Victoria y desactivamos los botones
-            if (condicionVictoria == true)
+            if (victoria == true)
             {
                 MessageBox.Show("Todos los números organizados, Felicitaciones!", "Victoria Alcanzada!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 InactivaBotones();
@@ -406,13 +262,13 @@ namespace OrdenaNumeros
             for (int i = 0; i < totalFilas; i++)
                 for (int j = 0; j < totalColumnas; j++)
                 {
-                    if (matrizValores[i, j] == valoresEsperados[i, j])
+                    if (logicaJuego.MatrizValores[i, j] == valoresEsperados[i, j])
                         matrizBotones[i, j].BackColor = Color.LightGreen;
                     else
                         matrizBotones[i, j].BackColor = Color.LightGray;
 
                     //El botón que tiene el 0 no deberá cambiar de color
-                    if(matrizValores[i, j]==0)
+                    if (logicaJuego.MatrizValores[i, j] == 0)
                         matrizBotones[i, j].BackColor = Color.LightGray;
                 }
         }
@@ -429,6 +285,23 @@ namespace OrdenaNumeros
             for (int i = 0; i < totalFilas; i++)
                 for (int j = 0; j < totalColumnas; j++)
                     matrizBotones[i, j].BackColor = Color.LightGray;
+        }
+
+        private void Actualizador()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (logicaJuego.MatrizValores[i, j] == 0)
+                        matrizBotones[i, j].Text = "";
+                    else
+                        matrizBotones[i, j].Text = logicaJuego.MatrizValores[i, j].ToString();
+                }
+            }
+
+            //Y valoramos la condición de victoria
+            EvaluaCondicionVictoria();
         }
     }
 }
---
+++ OrdenaNumeros/Logica.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenaNumeros
+{
+    public class Logica
+    {
+        private int[,] matrizValores;
+        private int posicionFila, posicionColumna;
+
+        public int[,] MatrizValores
+        {
+            get
+            {
+                return matrizValores;
+            }
+        }
+        public int PosicionFila
+        {
+            get
+            {
+                return posicionFila;
+            }
+        }
+        public int PosicionColumna
+        {
+            get
+            {
+                return posicionColumna;
+            }
+        }
+
+        public Logica()
+        {
+            posicionFila = 0;
+            posicionColumna = 0;
+
+            matrizValores = new int[4, 4];
+            InicializaMatrizValores();
+
+        }
+
+        public int[,] InicializaMatrizValores()
+        {
+            int valor = 0;
+
+
+            //Inicialmente se asignan los números del 0 al 15 en toda la matriz
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    matrizValores[i, j] = valor;
+                    valor++;
+                }
+            }
+
+            //Luego, procedemos a cambiar los valores de posición de manera aleatoria
+
+            Random aleatorio = new Random();
+            int posicionHorizontal, posicionVertical, valorTemporal;
+
+            //Aqui desordenamos la matriz, calculando posiciones horizontales y
+            //verticales dentro de la matriz
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    valorTemporal = matrizValores[i, j];
+                    posicionHorizontal = aleatorio.Next(4);
+                    posicionVertical = aleatorio.Next(4);
+
+                    matrizValores[i, j] = matrizValores[posicionHorizontal, posicionVertical];
+                    matrizValores[posicionHorizontal, posicionVertical] = valorTemporal;
+                }
+            }
+
+
+            int[,] referenciaOrdenada = new int[4, 4];
+            int[] numerosordenados = new int[16];
+            int valores = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    numerosordenados[valores++] = matrizValores[i, j];
+                }
+            }
+            int datoTemporal;
+            bool seHizoCambio = true;
+            while (seHizoCambio == true)
+            {
+                seHizoCambio = false;
+                for (int i = 0; i < numerosordenados.Length - 1; i++)
+                {
+                    if (numerosordenados[i] > numerosordenados[i + 1])
+                    {
+                        datoTemporal = numerosordenados[i + 1];
+                        numerosordenados[i + 1] = numerosordenados[i];
+                        numerosordenados[i] = datoTemporal;
+                        seHizoCambio = true;
+                    }
+                }
+            }
+            valores = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                    referenciaOrdenada[i, j] = numerosordenados[valores++];
+                }
+            }
+
+            return referenciaOrdenada;
+        }
+        public bool EvaluaCondicionVictoria()
+        {
+            //Partimos del supuesto que ya logramos la condición de victoria
+            bool condicionVictoria = true;
+
+            int valorBuscado = 0;
+
+            //Aqui recorremos la matriz de valores buscando para cada posición si el valor está en orden
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    //incrementamos el valor buscado
+                    valorBuscado++;
+
+                    //Si los valores son diferentes, entonces todavía necesitamos seguir jugando!!!
+                    if (matrizValores[i, j] != valorBuscado)
+                    {
+                        // Validamos si estamos en la última casilla, el valor existente es 0,
+                        // el valor buscado ya llegó a 16 y la condición de victoria sigue siendo true
+                        if (matrizValores[i, j] == 0 && valorBuscado == 16 && condicionVictoria == true)
+                            condicionVictoria = true;
+
+                        // De lo contrario, es porque estamos en cualquier otra casilla y los valores
+                        // Todavía no son iguales
+                        else
+                            condicionVictoria = false;
+                    }
+                }
+            }
+            return condicionVictoria;
+        }
+        public void EvaluaPosicion(int datofila, int datocolumna)
+        {
+
+            int valorTemporal = 0;
+
+            //Validamos el valor superior a donde presionamos si está el cero
+            if (datofila > 0)
+            {
+                if (matrizValores[datofila - 1, datocolumna] == 0)
+                {
+                    valorTemporal = matrizValores[datofila, datocolumna];
+                    matrizValores[datofila, datocolumna] = 0;
+                    matrizValores[datofila - 1, datocolumna] = valorTemporal;
+                }
+            }
+
+            //Validamos el valor inferior a donde presionamos si está el cero
+            if (datofila < 3)
+            {
+                if (matrizValores[datofila + 1, datocolumna] == 0)
+                {
+                    valorTemporal = matrizValores[datofila, datocolumna];
+                    matrizValores[datofila, datocolumna] = 0;
+                    matrizValores[datofila + 1, datocolumna] = valorTemporal;
+                }
+            }
+
+            //Validamos el valor izquierdo a donde presionamos si está el cero
+            if (datocolumna > 0)
+            {
+                if (matrizValores[datofila, datocolumna - 1] == 0)
+                {
+                    valorTemporal = matrizValores[datofila, datocolumna];
+                    matrizValores[datofila, datocolumna] = 0;
+                    matrizValores[datofila, datocolumna - 1] = valorTemporal;
+                }
+            }
+
+            //Validamos el valor derecho a donde presionamos si está el cero
+            if (datocolumna < 3)
+            {
+                if (matrizValores[datofila, datocolumna + 1] == 0)
+                {
+                    valorTemporal = matrizValores[datofila, datocolumna];
+                    matrizValores[datofila, datocolumna] = 0;
+                    matrizValores[datofila, datocolumna + 1] = valorTemporal;
+                }
+            }
+        }
+        public void EvaluaBoton(int numeroBoton, int datoFila, int datoColumna)
+        {
+            posicionFila = datoFila;
+            posicionColumna = datoColumna;
+            int numero = numeroBoton;
+            //Aqui evaluamos en la matrizValores, la posición correspondiente al botón presionado
+            EvaluaPosicion(datoFila, datoColumna);
+
+        }
+    }
+}
